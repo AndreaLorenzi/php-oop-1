f7114b2f6c19f1bd585e03068f0a1e64c3576109
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-19 15:56:08 +0200

    function

--- index.php
+++ index.php
@@ -5,6 +5,56 @@ class movies
     public $genre;
     public $year;
     public $production;
-}
 
-// contruct
+
+    // contruct
+    function __construct($_title, $_genre, $_year, $_production)
+    {
+        $this->title = $_title;
+        $this->genre = $_genre;
+        $this->year = $_year;
+        $this->production = $_production;
+    }
+
+
+    function setTitle($_title)
+    {
+        $this->title;
+    }
+    function getTitle()
+    {
+        return $this->title;
+    }
+
+    function setGenre($_genre)
+    {
+        $this->genre;
+    }
+    function getGenre()
+    {
+        return $this->genre;
+    }
+
+    function setYear($_year)
+    {
+        $this->year;
+    }
+    function getYear()
+    {
+        return $this->year;
+    }
+
+    function setProduction($_production)
+    {
+        $this->production;
+    }
+    function getProduction()
+    {
+        return $this->production;
+    }
+
+    function getInformation()
+    {
+        return 'Titolo: ' . $this->getTitle() . '</br>' . 'Genere: ' . $this->getGenre() . '</br>' . 'Anno: ' . $this->getYear() . '</br>' . 'Regista: ' . $this->getProduction() . '</br>';
+    }
+}
